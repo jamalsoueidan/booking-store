30508e8fd083e091d86a5247ac13cd5789060127
Jamal Soueidan <jamal@soueidan.com>
2023-11-06 20:33:57 +0100

    add api

---
+++ app/lib/api/mutator/query-client.ts
@@ -0,0 +1,84 @@
+// custom-instance.ts
+
+import {ValidatorError} from 'remix-validated-form';
+import {Errors} from '../model';
+
+const baseURL = 'https://booking-shopify-api.azurewebsites.net/api'; // use your own URL here or environment variable
+
+export type ErrorType<ErrorData> = ErrorData;
+// In case you want to wrap the body type (optional)
+// (if the custom instance is processing data before sending it, like changing the case for example)
+export type BodyType<BodyData> = BodyData & {headers?: any};
+
+function paramsToQueryString(params: Record<string, string | Date>) {
+  if (!params) {
+    return '';
+  }
+  const queryString = Object.keys(params)
+    .map((key) => {
+      let value = params[key];
+      if (value instanceof Date) {
+        value = value.toJSON();
+      }
+      return `${encodeURIComponent(key)}=${encodeURIComponent(value)}`;
+    })
+    .join('&');
+
+  return `?${queryString}`;
+}
+
+function convertErrorMessage(errorObject: Errors): ValidatorError {
+  const result = errorObject.errors.reduce((accumulator, error) => {
+    // Iterate over the paths array and create keys for each path
+    error.path.forEach((path) => {
+      // If the key doesn't exist, add it to the accumulator with the error message
+      if (!accumulator[path]) {
+        accumulator[path] = error.message;
+      }
+    });
+    return accumulator;
+  }, {} as Record<string, string>);
+
+  return {fieldErrors: result};
+}
+export function isValidatorError(obj: any): obj is ValidatorError {
+  return obj && Object.keys((obj as ValidatorError).fieldErrors).length > 0;
+}
+
+export function isError(obj: any): obj is Errors {
+  return obj && (obj as Errors).success === false;
+}
+
+export const queryClient = async <T>({
+  url,
+  method,
+  headers,
+  params,
+  data,
+}: {
+  url: string;
+  method: 'get' | 'post' | 'put' | 'delete' | 'patch';
+  params?: any;
+  headers?: any;
+  data?: BodyType<unknown>;
+  responseType?: string;
+}): Promise<T> => {
+  const response = await fetch(
+    `${baseURL}${url.replace(/gid:\/\/shopify\/[A-Za-z]+\//, '')}` +
+      paramsToQueryString(params),
+    {
+      method,
+      ...(data ? {body: JSON.stringify(data)} : {}),
+    },
+  );
+
+  const responseJson = await response.json();
+  if (isError(responseJson)) {
+    console.log(JSON.stringify(responseJson));
+    throw convertErrorMessage(responseJson);
+  }
+
+  return responseJson as T;
+};
+
+export default queryClient;
---
+++ orval.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  api: {
+    output: {
+      mode: 'split',
+      schemas: 'app/lib/api/model',
+      client: 'axios',
+      target: 'app/lib/api/',
+      mock: false,
+      override: {
+        useDates: true,
+        query: {
+          useQuery: false,
+          useInfinite: false,
+        },
+        mutator: {
+          path: './app/lib/api/mutator/query-client.ts',
+          name: 'queryClient',
+        },
+      },
+    },
+    input: {
+      target: './openapi.yaml',
+    },
+    hooks: {
+      afterAllFilesWrite: 'prettier --write',
+    },
+  },
+  zod: {
+    output: {
+      mode: 'split',
+      client: 'zod',
+      target: 'app/lib/zod/',
+    },
+    input: {
+      target: './openapi.yaml',
+    },
+    hooks: {
+      afterAllFilesWrite: 'prettier --write',
+    },
+  },
+};
