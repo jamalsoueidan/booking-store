54f5503e126a6bbc8b746636e12a88968285702d
Jamal Soueidan <jamal@soueidan.com>
2023-11-07 20:40:35 +0100

    create location done with address-autocomplete-input component

---
+++ app/components/AddressAutocompleteInput.tsx
@@ -0,0 +1,122 @@
+import {type conform} from '@conform-to/react';
+import {Combobox, TextInput, useCombobox} from '@mantine/core';
+import {useFetcher} from '@remix-run/react';
+import {useEffect, useState} from 'react';
+import {type ApiAutoCompleteProposal} from '~/routes/($locale).api.autocomplete';
+
+export function AddressAutocompleteInput({
+  label,
+  placeholder,
+  defaultValue,
+  ...config
+}: {
+  label: string;
+  placeholder: string;
+} & ReturnType<typeof conform.input>) {
+  const fetcher = useFetcher<Array<ApiAutoCompleteProposal>>();
+  const [query, setQuery] = useState({
+    q: '',
+    type: 'adresse',
+    caretpos: 2,
+    supplerendebynavn: true,
+    stormodtagerpostnumre: true,
+    multilinje: false,
+    fuzzy: '',
+    adgangsadresseid: null,
+    startFra: null,
+  });
+
+  const combobox = useCombobox();
+  const [value, setValue] = useState(defaultValue ?? '');
+
+  useEffect(() => {
+    if (query.q !== '' && query.q.length > 1) {
+      fetcher.load(`/api/autocomplete?${objectToQueryString(query)}`);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query]);
+
+  const options = fetcher.data?.map((item) => (
+    <Combobox.Option value={item.forslagstekst} key={item.forslagstekst}>
+      {item.forslagstekst}
+    </Combobox.Option>
+  ));
+
+  const onOptionSubmit = (value: string) => {
+    const address: ApiAutoCompleteProposal | undefined = fetcher.data?.find(
+      (v) => v.forslagstekst === value,
+    );
+
+    if (!address) {
+      return;
+    }
+
+    if ((address.data as any).adgangsadresseid !== undefined) {
+      combobox.closeDropdown();
+      return setValue(address.tekst);
+    }
+
+    if (
+      fetcher.data &&
+      fetcher.data.length === 1 &&
+      address.type !== 'vejnavn'
+    ) {
+      combobox.closeDropdown();
+      return setValue(address.tekst);
+    } else {
+      setQuery((prev) => ({
+        ...prev,
+        q: address.tekst,
+        caretpos: address.caretpos,
+        startfra: address.type === 'vejnavn' ? 'adgangsadresse' : null,
+      }));
+    }
+  };
+
+  const showDropdown = () => {
+    if (fetcher.data && fetcher.data.length > 0) {
+      combobox.openDropdown();
+    }
+  };
+
+  return (
+    <Combobox
+      onOptionSubmit={onOptionSubmit}
+      withinPortal={false}
+      store={combobox}
+    >
+      <Combobox.Target>
+        <TextInput
+          label={label} // Hvor vil du kører fra? Hvor skal kunden køre til?
+          value={value}
+          placeholder={placeholder}
+          {...config}
+          onChange={(event) => {
+            setValue(event.currentTarget.value);
+            setQuery({...query, q: event.currentTarget.value});
+            if (event.currentTarget.value.length === 0) {
+              return combobox.closeDropdown();
+            }
+            combobox.openDropdown();
+          }}
+          onClick={showDropdown}
+          onFocus={showDropdown}
+          onBlur={() => combobox.closeDropdown()}
+        />
+      </Combobox.Target>
+
+      <Combobox.Dropdown>
+        <Combobox.Options>
+          {!options ? <Combobox.Empty>Nothing found</Combobox.Empty> : options}
+        </Combobox.Options>
+      </Combobox.Dropdown>
+    </Combobox>
+  );
+}
+
+const objectToQueryString = (obj: any) => {
+  return Object.keys(obj)
+    .filter((key) => obj[key] !== null && obj[key] !== undefined)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(obj[key]))
+    .join('&');
+};
--- app/routes/($locale).account.locations.create.tsx
+++ app/routes/($locale).account.locations.create.tsx
@@ -1,3 +1,212 @@
-export default function AccountLocationsCreate() {
-  return <>asd</>;
+import {Form, Link, useActionData} from '@remix-run/react';
+import {
+  json,
+  redirect,
+  type ActionFunctionArgs,
+  type LoaderFunctionArgs,
+} from '@shopify/remix-oxygen';
+
+import {getCustomer} from '~/lib/get-customer';
+import {customerLocationCreateBody} from '~/lib/zod/bookingShopifyApi';
+
+import {
+  conform,
+  useForm,
+  useInputEvent,
+  type FieldConfig,
+} from '@conform-to/react';
+import {parse} from '@conform-to/zod';
+import {
+  ActionIcon,
+  Divider,
+  Flex,
+  Radio,
+  Stack,
+  TextInput,
+  Title,
+} from '@mantine/core';
+import {IconArrowLeft} from '@tabler/icons-react';
+import {useRef, useState} from 'react';
+import {AddressAutocompleteInput} from '~/components/AddressAutocompleteInput';
+import {SubmitButton} from '~/components/form/SubmitButton';
+import {getBookingShopifyApi} from '~/lib/api/bookingShopifyApi';
+
+export async function loader({context}: LoaderFunctionArgs) {
+  const customerAccessToken = await context.session.get('customerAccessToken');
+  if (!customerAccessToken) {
+    return redirect('/account/login');
+  }
+  return json({});
+}
+
+export const action = async ({request, context}: ActionFunctionArgs) => {
+  const customerAccessToken = await context.session.get('customerAccessToken');
+  const customer = await getCustomer({context, customerAccessToken});
+
+  const formData = await request.formData();
+  const submission = parse(formData, {schema: customerLocationCreateBody});
+
+  if (submission.intent !== 'submit' || !submission.value) {
+    return json(submission);
+  }
+
+  try {
+    const response = await getBookingShopifyApi().customerLocationCreate(
+      customer.id,
+      submission.value,
+    );
+
+    return redirect(`/account/locations/${response.payload._id}/edit`);
+  } catch (error) {
+    return json(submission);
+  }
+};
+
+export default function Component() {
+  const lastSubmission = useActionData<typeof action>();
+
+  const [form, fields] = useForm({
+    lastSubmission,
+    defaultValue: {
+      name: '',
+      locationType: 'origin',
+      fullAddress: '',
+      originType: 'home',
+      distanceHourlyRate: 500,
+      fixedRatePerKm: 20,
+      distanceForFree: 4,
+      minDriveDistance: 0,
+      maxDriveDistance: 300,
+      startFee: 0,
+    },
+  });
+
+  const [locationType, setLocationType] = useState(
+    fields.locationType.defaultValue,
+  );
+
+  return (
+    <>
+      <Flex direction={'row'} align={'center'}>
+        <Link to="/account/locations">
+          <ActionIcon
+            variant="transparent"
+            size="xl"
+            aria-label="Back"
+            color="black"
+          >
+            <IconArrowLeft style={{width: '70%', height: '70%'}} stroke={1.5} />
+          </ActionIcon>
+        </Link>
+        <Title>Redigere din profil</Title>
+      </Flex>
+      <Divider my="md" />
+
+      <Form method="POST" {...form.props}>
+        <Stack>
+          <RadioGroup onChange={setLocationType} {...fields.locationType} />
+          <TextInput
+            label="Navn"
+            placeholder="BySisters"
+            {...conform.input(fields.name)}
+          />
+          <AddressAutocompleteInput
+            label={
+              locationType === 'destination'
+                ? 'Hvor vil du kører fra?'
+                : 'Hvor skal kunden køre til?'
+            }
+            placeholder="Sigridsvej 45, 8220 Brabrand"
+            {...conform.input(fields.fullAddress)}
+          />
+          <input type="hidden" {...conform.input(fields.originType)} />
+          {locationType === 'destination' ? (
+            <>
+              <TextInput name="startFee" label="Udgifter for turen" />
+
+              <TextInput
+                label="Timepris for kørsel"
+                {...conform.input(fields.distanceHourlyRate)}
+              />
+
+              <TextInput
+                label="Pris pr. kilometer"
+                {...conform.input(fields.fixedRatePerKm)}
+              />
+
+              <TextInput
+                label="Afstanden der køres gratis, inden takstberegningen påbegyndes."
+                {...conform.input(fields.distanceForFree)}
+              />
+
+              <TextInput
+                label="Minimum der skal køres for at acceptere en kørselsopgave"
+                {...conform.input(fields.minDriveDistance)}
+              />
+
+              <TextInput
+                label="Maximum der køres"
+                {...conform.input(fields.maxDriveDistance)}
+              />
+            </>
+          ) : (
+            <>
+              <input type="hidden" name="distanceForFree" value="0" />
+              <input type="hidden" name="fixedRatePerKm" value="0" />
+              <input type="hidden" name="distanceHourlyRate" value="0" />
+              <input type="hidden" name="minDriveDistance" value="0" />
+              <input type="hidden" name="maxDriveDistance" value="500" />
+              <input type="hidden" name="startFee" value="0" />
+            </>
+          )}
+
+          <SubmitButton>Tilføj</SubmitButton>
+        </Stack>
+      </Form>
+    </>
+  );
+}
+
+function RadioGroup({
+  onChange,
+  ...config
+}: FieldConfig<string> & {onChange: (value: string) => void}) {
+  const [value, setValue] = useState(config.defaultValue ?? '');
+
+  const shadowInputRef = useRef<HTMLInputElement>(null);
+
+  const control = useInputEvent({
+    ref: shadowInputRef,
+    onReset: () => setValue(config.defaultValue ?? ''),
+  });
+
+  // https://conform.guide/checkbox-and-radio-group
+  return (
+    <>
+      <input
+        ref={shadowInputRef}
+        {...conform.input(config, {hidden: true})}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <Radio.Group
+        label="Hvilken type location vil du oprette?"
+        value={value}
+        onChange={(value: string) => {
+          control.change(value);
+          onChange(value);
+        }}
+      >
+        <Radio
+          mt="xs"
+          mb="xs"
+          label="Opret en fast arbejdslokation."
+          value="origin"
+        />
+        <Radio
+          label="Opret en mobil arbejdslokation (Du vil kører til kunden)."
+          value="destination"
+        />
+      </Radio.Group>
+    </>
+  );
 }
