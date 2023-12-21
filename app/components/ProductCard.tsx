d30a1f7612aa014e497cac416010145365210506
Jamal Soueidan <jamal@soueidan.com>
2023-12-21 22:23:01 +0300

    chore(ProductCard.module.css): remove hover effect on .card class feat(ProductCard.tsx): change Card.Group to Stack component for better alignment and spacing feat(ProductCard.tsx): change aspectRatio of Image component to "1/2" for better visual display feat(ProductCard.tsx): remove product.handle Text component and adjust layout of product.title Text component and Badge component using Stack component

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,4 +1,4 @@
-import {Badge, Card, Group, Text} from '@mantine/core';
+import {Badge, Card, Stack, Text} from '@mantine/core';
 import {Link} from '@remix-run/react';
 import {Image, Money} from '@shopify/hydrogen';
 import {type ProductItemFragment} from 'storefrontapi.generated';
@@ -29,7 +29,7 @@ export function ProductCard({
         <Card.Section className={classes.imageSection}>
           <Image
             alt={product.featuredImage.altText || product.title}
-            aspectRatio="1/3"
+            aspectRatio="1/2"
             data={product.featuredImage}
             loading={loading}
             sizes="(min-width: 45em) 400px, 100vw"
@@ -37,17 +37,12 @@ export function ProductCard({
         </Card.Section>
       )}
 
-      <Group justify="space-between" mt="md">
-        <div>
-          <Text fw={500}>{product.title}</Text>
-          <Text fz="xs" c="dimmed">
-            {product.handle}
-          </Text>
-        </div>
+      <Stack mt="md" gap="xs">
+        <Text fw={500}>{product.title}</Text>
         <Badge variant="outline">
           <Money data={product.priceRange.minVariantPrice} />
         </Badge>
-      </Group>
+      </Stack>
     </Card>
   );
 }
