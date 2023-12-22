a134d1489705ae7b20bf65a2177a2cfe6b17f9bd
Jamal Soueidan <jamal@soueidan.com>
2023-12-22 13:52:25 +0300

    refactor(ArtistProduct.tsx): - Change width and height of the image in Badge component to a fixed value of 18 - Remove redundant size prop from Badge component - Remove redundant py prop from Badge component - Remove conditional rendering of ActionIcon component based on isMobile - Change size prop of Title component to a fixed value of 24 - Change mt prop of Title component to a fixed value of 12 - Change size prop of Text component to a fixed value of md - Change fz prop of Text component to a fixed value of xs - Change fz prop of Text component to a fixed value of xs - Change size prop of Badge component to a fixed value of lg - Change fz prop of Text component to a fixed value of xs

--- app/components/artist/ArtistProduct.tsx
+++ app/components/artist/ArtistProduct.tsx
@@ -48,42 +48,30 @@ export function ArtistProduct({product, services}: ArtistProductProps) {
                 'reshot-icon-beauty-mirror'
               }.svg`}
               style={{
-                width: rem(isMobile ? 12 : 18),
-                height: rem(isMobile ? 12 : 18),
+                width: rem(18),
+                height: rem(18),
               }}
               alt="ok"
             />
           }
+          size="xs"
+          fz="xs"
           radius="md"
           variant="outline"
           color="#ebeaeb"
-          size={isMobile ? 'xs' : 'sm'}
           bg="#f7f7f7"
-          fz="xs"
           c="gray.5"
-          py={isMobile ? 'xs' : 'md'}
         >
           {parseTE(product.collections.nodes[0].title)}
         </Badge>
-        {!isMobile && (
-          <ActionIcon
-            variant="outline"
-            color="black"
-            radius="lg"
-            size="md"
-            aria-label="Settings"
-          >
-            <IconArrowRight
-              style={{width: '70%', height: '70%'}}
-              stroke={1.5}
-            />
-          </ActionIcon>
-        )}
+        <ActionIcon variant="outline" color="black" radius="lg" size="md">
+          <IconArrowRight style={{width: '70%', height: '70%'}} stroke={1.5} />
+        </ActionIcon>
       </Group>
       <Title
         order={2}
-        size={rem(isMobile ? 16 : 24)}
-        mt={rem(isMobile ? 8 : 16)}
+        size={rem(24)}
+        mt={rem(12)}
         mb={rem(4)}
         fw={600}
         lts=".5px"
@@ -97,7 +85,7 @@ export function ArtistProduct({product, services}: ArtistProductProps) {
         justify="flex-start"
         style={{flexGrow: 1, position: 'relative'}}
       >
-        <Text c="dimmed" size={isMobile ? 'xs' : 'md'} fw={400} lineClamp={2}>
+        <Text c="dimmed" size="md" fw={400} lineClamp={2}>
           {artistService?.description ||
             product.description ||
             'ingen beskrivelse'}
@@ -109,12 +97,12 @@ export function ArtistProduct({product, services}: ArtistProductProps) {
       </Card.Section>
 
       <Group justify="space-between" gap="0">
-        <Text c="dimmed" fz={isMobile ? 11 : 'xs'} tt="uppercase" fw={700}>
+        <Text c="dimmed" fz="xs" tt="uppercase" fw={700}>
           {durationToTime(artistService?.duration ?? 0)}
         </Text>
 
         {artistService?.price && (
-          <Badge variant="light" color="gray" size={isMobile ? 'sm' : 'lg'}>
+          <Badge variant="light" color="gray" size="lg">
             <Money data={artistService?.price as any} />
           </Badge>
         )}
