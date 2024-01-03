f2babb0aa69bdfe131327f9a46272af9f1cb2032
Jamal Soueidan <jamal@soueidan.com>
2024-01-04 01:39:42 +0300

    fix(entry.server.tsx): update Content-Security-Policy header with additional domains

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -34,7 +34,7 @@ export default async function handleRequest(
   responseHeaders.set('Content-Type', 'text/html');
   const newHeader = header.replace(
     'https://shopify.com',
-    'https://shopify.com https://www.google.com https://notify.bugsnag.com shopify-chat.shopifyapps.com',
+    'https://shopify.com https://www.google.com notify.bugsnag.com https://js.hcaptcha.com https://newassets.hcaptcha.com https://api.hcaptcha.com https://api2.hcaptcha.com shopify-chat.shopifyapps.com',
   );
   responseHeaders.set('Content-Security-Policy', newHeader);
 
