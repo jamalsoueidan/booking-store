4ab567a81c5856a0f493e450c48309f650ae0278
Jamal Soueidan <jamal@soueidan.com>
2024-03-10 17:33:24 +0100

    Refactor forms to use new @conform-to/react and @conform-to/zod APIs, replace conform with getInputProps, getTextareaProps, getFormProps, and update MultiTags component to use useField and useCallback. Fix action handlers to use parseWithZod and submission.reply() for error handling. Add Facebook input to social form.

--- app/components/form/MultiTags.tsx
+++ app/components/form/MultiTags.tsx
@@ -1,15 +1,9 @@
-import {
-  list,
-  requestIntent,
-  useFieldList,
-  type FieldConfig,
-} from '@conform-to/react';
+import {useField, type FieldMetadata} from '@conform-to/react';
 import {MultiSelect} from '@mantine/core';
-import React, {useRef} from 'react';
+import React, {useCallback} from 'react';
 
 interface MultiTagsProps {
-  field: FieldConfig<Array<any>>;
-  name: string;
+  field: FieldMetadata<Array<string>>;
   data: {label: string; value: string}[];
   label: string;
   placeholder: string;
@@ -17,51 +11,39 @@ interface MultiTagsProps {
 
 export const MultiTags: React.FC<MultiTagsProps> = ({
   field,
-  name,
   data,
   label,
   placeholder,
 }) => {
-  const ref = useRef<HTMLFieldSetElement>(null);
-  const fieldList = useFieldList<Array<string>>(ref, field);
+  const [, form] = useField(field.name);
+  const list = field.getFieldList();
+  const defaultValue = list.map((item) => item.initialValue) as string[];
 
-  const handleChange = (value: string[]) => {
-    fieldList.forEach((item, index) => {
-      const exist = value.findIndex((l) => l === item.defaultValue);
-      if (exist === -1) {
-        requestIntent(ref.current?.form, list.remove(name, {index}));
-      }
-    });
-
-    value.forEach((itemValue, index) => {
-      const exist = fieldList.findIndex(
-        (item) => item.defaultValue === itemValue,
-      );
-
-      if (exist === -1) {
-        requestIntent(
-          ref.current?.form,
-          list.insert(name, {defaultValue: itemValue, index}),
-        );
-      }
-    });
-  };
+  const handleChange = useCallback(
+    (value: string[]) => {
+      form.update({
+        name: field.name,
+        value,
+      });
+    },
+    [field.name, form],
+  );
 
   return (
-    <fieldset ref={ref}>
+    <fieldset>
       <MultiSelect
         data={data}
         label={label}
+        defaultValue={defaultValue}
         placeholder={placeholder}
-        defaultValue={field.defaultValue}
         onChange={handleChange}
       />
-      {/* Render hidden inputs for each selected item */}
-      {fieldList.map((item) => (
+
+      {list.map((item) => (
         <input
-          type="hidden"
+          hidden
           key={item.key}
-          defaultValue={item.defaultValue}
+          defaultValue={item.initialValue}
           name={item.name}
         />
       ))}
