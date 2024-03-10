6771c48b62119f358b54847997ace37a37846d5e
Jamal Soueidan <jamal@soueidan.com>
2024-03-10 03:40:17 +0100

    Refactor RadioGroup components to use useInputControl and remove local state management

--- app/components/form/RadioGroupVariantProducts.tsx
+++ app/components/form/RadioGroupVariantProducts.tsx
@@ -1,28 +1,21 @@
-import {type FieldConfig} from '@conform-to/react';
-import React, {useEffect, useState} from 'react';
+import {type FieldMetadata} from '@conform-to/react';
+import React, {useEffect} from 'react';
 
 import {useFetcher} from '@remix-run/react';
 import {parseGid} from '@shopify/hydrogen';
 import {type ProductVariantFragment} from 'storefrontapi.generated';
-import type {CustomerProductUpsertBodySelectedOptions} from '~/lib/api/model';
 import {RadioGroup} from './RadioGroup';
 
 interface RadioRadioVariantsProductProps {
   label: string;
   productId: string;
-  field: FieldConfig<string>;
+  field: FieldMetadata<string> | FieldMetadata<number>;
 }
 
 export const RadioGroupVariantsProduct: React.FC<
   RadioRadioVariantsProductProps
 > = ({productId, label, field}: RadioRadioVariantsProductProps) => {
   const fetcher = useFetcher<ProductVariantFragment[]>();
-
-  const [selectedOptions, setSelectedOptions] =
-    useState<CustomerProductUpsertBodySelectedOptions>({
-      name: '',
-      value: '',
-    });
 
   const data = fetcher.data
     ?.sort((a, b) => parseFloat(a.price.amount) - parseFloat(b.price.amount))
