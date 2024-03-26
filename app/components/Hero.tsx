aff1b7f9c89db5bbb19e5bb42375964de6c1b99d
Jamal Soueidan <jamal@soueidan.com>
2024-03-26 02:27:59 +0100

    Refactor Hero component and update FeaturedArtists and RecommendedTreatments sections
     • Remove Hero.module.css and inline styles with Mantine components
     • Replace Flex with SimpleGrid and Stack for layout in Hero.tsx
     • Add animations and state management for card stack in Hero
     • Simplify FeaturedArtists with SimpleGrid and add professions
     • Update RecommendedTreatments to use Suspense and Autoplay carousel
     • Remove unused imports and components in route file

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,33 +1,128 @@
-import {Button, Flex, Title} from '@mantine/core';
+import {
+  Button,
+  Container,
+  Flex,
+  rem,
+  SimpleGrid,
+  Stack,
+  Text,
+  Title,
+} from '@mantine/core';
 import {Link} from '@remix-run/react';
-import classes from './Hero.module.css';
+import {IconArrowRightBar} from '@tabler/icons-react';
+import {motion} from 'framer-motion';
+import {useState} from 'react';
 
 export function Hero() {
   return (
-    <div className={classes.root}>
-      <Flex justify="center" direction="column" className={classes.flex}>
-        <Title
-          ta="center"
-          lts="1px"
-          className={classes.title}
-          textWrap="balance"
-        >
-          BySisters er en skønhedsplatform hvor du kan finde professional
-          artister i nærheden af dig!
-        </Title>
+    <Container size="lg" py="xl" h="100%">
+      <SimpleGrid cols={2} spacing="xl">
+        <Stack align="flex-start">
+          <Title order={1} lts=".5px" size={rem(36)}>
+            Start din rejse med BySisters
+          </Title>
+          <Text c="dimmed" size="xl" fw="500">
+            Opdag og book skønhedseksperter nemt med vores skræddersyede
+            løsninger.
+          </Text>
 
-        <Flex justify="center">
-          <Button
-            variant="filled"
-            color="orange"
-            component={Link}
-            to="/artists"
-            className={classes.button}
-          >
-            Find en skønhedsekspert
-          </Button>
-        </Flex>
-      </Flex>
-    </div>
+          <Flex justify="center">
+            <Button
+              variant="filled"
+              color="orange"
+              component={Link}
+              to="/artists"
+              size="lg"
+              radius="xl"
+            >
+              Find en skønhedsekspert
+            </Button>
+
+            <Button
+              variant="subtle"
+              color="orange"
+              component={Link}
+              to="/artists"
+              size="lg"
+              radius="xl"
+              rightSection={<IconArrowRightBar />}
+            >
+              Start din skønhedskarrier
+            </Button>
+          </Flex>
+        </Stack>
+        <div>
+          <CardStack />
+        </div>
+      </SimpleGrid>
+    </Container>
   );
 }
+
+const CARD_COLORS = ['#266678', '#cb7c7a', ' #36a18b', '#cda35f', '#747474'];
+const CARD_OFFSET = 30;
+const SCALE_FACTOR = 0.06;
+
+const CardStack = () => {
+  const [cards, setCards] = useState(CARD_COLORS);
+
+  const moveToEnd = () => {
+    setCards((currentCards) => {
+      if (currentCards.length > 1) {
+        const [firstCard, ...restCards] = currentCards;
+        return [...restCards, firstCard];
+      }
+      return currentCards;
+    });
+  };
+
+  return (
+    <div
+      style={{
+        position: 'relative',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <ul
+        style={{
+          position: 'relative',
+          width: '350px',
+          height: '220px',
+        }}
+      >
+        {cards.map((color, index) => {
+          const canDrag = index === 0;
+
+          return (
+            <motion.li
+              key={color}
+              style={{
+                position: 'absolute',
+                width: '350px',
+                height: '220px',
+                borderRadius: '8px',
+                listStyle: 'none',
+                transformOrigin: 'center left',
+                backgroundColor: color,
+                cursor: canDrag ? 'grab' : 'auto',
+              }}
+              animate={{
+                right: index * -CARD_OFFSET,
+                scale: 1 - index * SCALE_FACTOR,
+                zIndex: CARD_COLORS.length - index,
+              }}
+              drag={canDrag ? 'x' : false}
+              dragConstraints={{
+                left: 0,
+                right: 0,
+              }}
+              onDragEnd={() => moveToEnd()}
+            />
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
