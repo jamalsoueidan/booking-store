48ed2916d947c3d7555e90949f512d12915491ea
Jamal Soueidan <jamal@soueidan.com>
2024-04-09 18:18:27 +0200

    add project id

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'cypress';
 
 export default defineConfig({
   e2e: {
+    projectId: 'm8irxe',
     baseUrl: 'http://localhost:3000',
     experimentalStudio: true,
     setupNodeEvents(on, config) {
