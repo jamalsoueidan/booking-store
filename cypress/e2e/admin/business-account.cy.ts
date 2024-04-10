3fa266c38d4905d54d59624d36d2d4f127bdde05
Jamal Soueidan <jamal@soueidan.com>
2024-04-10 08:39:32 +0200

    Refactor Cypress tests to use UI actions for login instead of custom command

--- cypress/e2e/admin/business-account.cy.ts
+++ cypress/e2e/admin/business-account.cy.ts
@@ -3,10 +3,31 @@ function generateRandomPhoneNumber() {
   return '8080' + randomNumber.toString();
 }
 
-describe('login', () => {
+describe('business account', () => {
+  beforeEach(() => {
+    cy.intercept(
+      'POST',
+      '/account/login?_data=routes%2F%28%24locale%29.account_.login',
+    ).as('loginRequest');
+
+    cy.intercept('GET', '/account?_data=routes%2F%28%24locale%29.account').as(
+      'login',
+    );
+  });
+
   it('passes', () => {
     cy.fixture('loginData.json').then((data) => {
-      cy.login(data.email, data.password);
+      //login
+      cy.visit('/');
+      cy.get('[data-cy="login-button"]').click();
+      cy.wait('@login');
+      cy.url().should('include', '/account/login');
+      cy.get('[data-cy="email-input"]').clear();
+      cy.get('[data-cy="email-input"]').type(data.email);
+      cy.get('[data-cy="password-input"]').should('be.enabled').clear();
+      cy.get('[data-cy="password-input"]').type(data.password);
+      cy.get('[data-cy="login-button"]').click();
+      cy.wait('@loginRequest');
 
       cy.get('[data-cy="welcome-title"]').should(
         'have.text',
--- cypress/e2e/admin/login.cy.ts
+++ cypress/e2e/admin/login.cy.ts
@@ -1,7 +1,28 @@
 describe('login', () => {
+  beforeEach(() => {
+    cy.intercept(
+      'POST',
+      '/account/login?_data=routes%2F%28%24locale%29.account_.login',
+    ).as('loginRequest');
+
+    cy.intercept('GET', '/account?_data=routes%2F%28%24locale%29.account').as(
+      'login',
+    );
+  });
+
   it('passes', () => {
     cy.fixture('loginData.json').then((data) => {
-      cy.login(data.email, data.password);
+      cy.visit('/');
+      cy.get('[data-cy="login-button"]').click();
+      cy.wait('@login');
+      cy.url().should('include', '/account/login');
+      cy.get('[data-cy="email-input"]').clear();
+      cy.get('[data-cy="email-input"]').type(data.email);
+      cy.get('[data-cy="password-input"]').should('be.enabled').clear();
+      cy.get('[data-cy="password-input"]').type(data.password);
+      cy.get('[data-cy="login-button"]').click();
+      cy.wait('@loginRequest');
+
       /* ==== Generated with Cypress Studio ==== */
       cy.get('[data-cy="welcome-title"]').should(
         'have.text',
