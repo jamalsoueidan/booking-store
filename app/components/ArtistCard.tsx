220724d77aee8260c196ad94c2c99e68d8134f7e
Jamal Soueidan <jamal@soueidan.com>
2024-04-16 02:43:54 +0200

    Remove Button import and usage from ArtistCard component

--- app/components/ArtistCard.tsx
+++ app/components/ArtistCard.tsx
@@ -1,4 +1,4 @@
-import {Box, Button, Image, Text, UnstyledButton} from '@mantine/core';
+import {Box, Image, Text, UnstyledButton} from '@mantine/core';
 import {Link} from '@remix-run/react';
 import {Image as ShopifyImage} from '@shopify/hydrogen';
 import type {User, UserTop} from '~/lib/api/model';
@@ -11,22 +11,17 @@ export const ArtistCard = ({artist}: {artist: User | UserTop}) => (
     to={`/artist/${artist.username}`}
     style={{borderRadius: '5%', border: '1px solid #f4f4f4'}}
   >
-    <div style={{position: 'relative'}}>
-      <Image
-        component={ShopifyImage}
-        sizes="(min-width: 45em) 50vw, 100vw"
-        aspectRatio="2/5"
-        src={artist.images?.profile?.url}
-        style={{
-          borderTopLeftRadius: '5%',
-          borderTopRightRadius: '5%',
-        }}
-        fallbackSrc="https://placehold.co/400x600?text=Ekspert"
-      />
-      <Button variant="default" size="xs" radius="lg">
-        Vis profile
-      </Button>
-    </div>
+    <Image
+      component={ShopifyImage}
+      sizes="(min-width: 45em) 50vw, 100vw"
+      aspectRatio="2/5"
+      src={artist.images?.profile?.url}
+      style={{
+        borderTopLeftRadius: '5%',
+        borderTopRightRadius: '5%',
+      }}
+      fallbackSrc="https://placehold.co/400x600?text=Ekspert"
+    />
     <Box p="xs" pb="xs" pt="6px">
       <Text fz="lg" fw={500} c="black">
         {artist.fullname}
