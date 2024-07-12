787b1eacff7d7f242a9ddb10c200fe86c053a41d
Jamal Soueidan <jamal@soueidan.com>
2024-07-12 05:20:07 +0200

    remove api files

--- app/routes/api.professions.ts
+++
@@ -1,6 +0,0 @@
-import {json} from '@shopify/remix-oxygen';
-import {professionImages} from '~/components/ProfessionButton';
-
-export async function loader() {
-  return json(Object.keys(professionImages).filter((key) => key !== 'all'));
-}
